7a3e22f30474409b8231afac98cc0917f7e392da
Javier <javier.fuentesm@hotmail.com>
2020-07-24 13:00:03 -0400

    Data Driven Testing (DDT) cn csv

--- search_ddt.py
+++ search_ddt.py
@@ -1,6 +1,18 @@
+import csv
 import unittest
-from selenium import webdriver
 from ddt import ddt, data, unpack
+from selenium import webdriver
+
+
+def get_data(filename):
+    rows = []
+    data_file = open(filename, 'r')
+    reader = csv.reader(data_file)
+    next(reader, None)
+
+    for row in reader:
+        rows.append(row)
+    return rows
 
 
 @ddt
@@ -13,7 +25,7 @@ class SearchDDT(unittest.TestCase):
         driver.maximize_window()
         driver.get('http://demo-store.seleniumacademy.com/')
 
-    @data(('dress', 6), ('music', 5))
+    @data(*get_data('testData.csv'))
     @unpack
     def test_search_dtt(self, search_value, expected_count):
         driver = self.driver
@@ -23,12 +35,21 @@ class SearchDDT(unittest.TestCase):
         search_field.submit()
 
         products = driver.find_elements_by_xpath('//h2[@class="product-name"]/a')
+        # print(f'Found {len(products)} productos')
+        #
+        # for product in products:
+        #     print(product.text)
+        #
+        # self.assertEqual(expected_count, len(products))
+
+        expected_count = int(expected_count)
+        if expected_count > 0:
+            self.assertEqual(expected_count, len(products))
+        else:
+            message = driver.find_element_by_class_name('note-msg')
+            self.assertEqual('Your search returns no results.', message)
+
         print(f'Found {len(products)} productos')
-
-        for product in products:
-            print(product.text)
-
-        self.assertEqual(expected_count, len(products))
 
     def tearDown(self):
         self.driver.quit()
